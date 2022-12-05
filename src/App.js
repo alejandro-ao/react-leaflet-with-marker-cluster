255f4a2cb392aa65750c36b5fe97ea7d0a03f672
Alejandro AO <aavila0707@gmail.com>
2022-12-05 15:46:33 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import "./styles.css";
+import "leaflet/dist/leaflet.css";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import MarkerClusterGroup from "react-leaflet-cluster";
+
+import { Icon, divIcon, point } from "leaflet";
+
+// create custom icon
+const customIcon = new Icon({
+  iconUrl: "https://cdn-icons-png.flaticon.com/512/447/447031.png",
+  iconSize: [38, 38] // size of the icon
+});
+
+// custom cluster icon
+const createClusterCustomIcon = function (cluster) {
+  return new divIcon({
+    html: `<span class="cluster-icon">${cluster.getChildCount()}</span>`,
+    className: "custom-marker-cluster",
+    iconSize: point(33, 33, true)
+  });
+};
+
+// markers
+const markers = [
+  {
+    geocode: [51.505, -0.09],
+    popUp: "Hello, I am pop up 1"
+  },
+  {
+    geocode: [51.504, -0.1],
+    popUp: "Hello, I am pop up 2"
+  },
+  {
+    geocode: [51.503, -0.09],
+    popUp: "Hello, I am pop up 3"
+  }
+];
+
+export default function App() {
+  return (
+    <MapContainer center={[51.505, -0.09]} zoom={13}>
+      {/* <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      /> */}
+      <TileLayer
+        attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg"
+      />
+
+      <MarkerClusterGroup
+        chunkedLoading
+        iconCreateFunction={createClusterCustomIcon}
+      >
+        {/* Mapping through the markers */}
+        {markers.map((marker) => (
+          <Marker position={marker.geocode} icon={customIcon}>
+            <Popup>{marker.popUp}</Popup>
+          </Marker>
+        ))}
+
+        {/* Hard coded markers */}
+        {/* <Marker position={[51.505, -0.09]} icon={customIcon}>
+          <Popup>This is popup 1</Popup>
+        </Marker>
+        <Marker position={[51.504, -0.1]} icon={customIcon}>
+          <Popup>This is popup 2</Popup>
+        </Marker>
+        <Marker position={[51.5, -0.09]} icon={customIcon}>
+          <Popup>This is popup 3</Popup>
+        </Marker>
+       */}
+      </MarkerClusterGroup>
+    </MapContainer>
+  );
+}
